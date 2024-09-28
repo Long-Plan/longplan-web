438fb721ef7f42b70265021cd4d605fd4ca13d7e
jmjirapat <jm_jirapat@hotmail.com>
2024-09-28 23:54:26 +0700

    impl: redirect localhost in dev env

--- src/common/apis/auth/queries.ts
+++ src/common/apis/auth/queries.ts
@@ -5,9 +5,22 @@ import { ApiRouteKey } from "../../constants/keys";
 export function signInQuery(code: string): Promise<string> {
   return new Promise((resolve, reject) => {
     coreApi
-      .post(ApiRouteKey.SignIn, null, {
-        params: new URLSearchParams({ code }),
-      })
+      .post(
+        ApiRouteKey.SignIn,
+        null,
+        IS_PRODUCTION_MODE
+          ? {
+              params: new URLSearchParams({
+                code,
+              }),
+            }
+          : {
+              params: new URLSearchParams({
+                code,
+                local: "true",
+              }),
+            }
+      )
       .then((res) => {
         resolve(res.data);
       })
