d7b6db243d1f4cf3414717e0f31d8888bc94be4c
JMjirapat <jirapat.prt@gmail.com>
2024-10-23 15:35:14 +0700

    fix: enrolled course dont load when first login

--- src/pages/oauth/OAuthPage.tsx
+++ src/pages/oauth/OAuthPage.tsx
@@ -6,39 +6,46 @@ import { getUserDataQuery, signInQuery } from "../../common/apis/auth/queries";
 import { coreApi } from "../../core/connections";
 import useAccountContext from "../../common/contexts/AccountContext";
 import { getDataOrNull } from "../../common/apis/selectors";
+import { getEnrolledCourses } from "../../common/apis/enrolled_course/queries";
+import useEnrolledCourseContext from "../../common/contexts/EnrolledCourseContext";
 
 function OAuthPage() {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const code = searchParams.get("code");
-  const navigate = useNavigate();
-  const { setAccountData, accountData } = useAccountContext();
+	const location = useLocation();
+	const searchParams = new URLSearchParams(location.search);
+	const code = searchParams.get("code");
+	const navigate = useNavigate();
+	const { setAccountData, accountData } = useAccountContext();
+	const { setEnrolledCoruseData } = useEnrolledCourseContext();
 
-  useEffect(() => {
-    if (accountData) {
-      navigate(ClientRouteKey.Home, { replace: true });
-      return;
-    }
+	useEffect(() => {
+		if (accountData) {
+			navigate(ClientRouteKey.Home, { replace: true });
+			return;
+		}
 
-    async function callbackHandler() {
-      if (!code) {
-        toast.error("Error: No code found in the URL.");
-        navigate(ClientRouteKey.Login, { replace: true });
-      } else {
-        const res = await signInQuery(code);
-        localStorage.setItem(LocalStorageKey.Auth, res.result ?? "");
-        coreApi.defaults.headers.common["Authorization"] = `Bearer ${
-          res.result ?? ""
-        }`;
-        const accountData = await getDataOrNull(getUserDataQuery);
-        setAccountData(accountData);
-        navigate(ClientRouteKey.Home, { replace: true });
-      }
-    }
+		async function callbackHandler() {
+			if (!code) {
+				toast.error("Error: No code found in the URL.");
+				navigate(ClientRouteKey.Login, { replace: true });
+			} else {
+				const res = await signInQuery(code);
+				localStorage.setItem(LocalStorageKey.Auth, res.result ?? "");
+				coreApi.defaults.headers.common["Authorization"] = `Bearer ${
+					res.result ?? ""
+				}`;
+				const accountData = await getDataOrNull(getUserDataQuery);
+				setAccountData(accountData);
+				getEnrolledCourses().then((data) => {
+					setEnrolledCoruseData(data.result ?? []);
+				});
+				navigate(ClientRouteKey.Home, { replace: true });
+			}
+		}
 
-    callbackHandler();
-  }, [accountData, code, navigate, setAccountData]);
-  return <div>Loading.. .</div>;
+		callbackHandler();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [accountData, code]);
+	return <div>Loading.. .</div>;
 }
 
 export default OAuthPage;
